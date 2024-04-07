2b8ecd833d8d02e631baa303edf5c6f1853b5f73
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-04-07 11:51:54 -0300

    feat: enable CPU profiling in hermes

--- cmd/hermes/hermes.go
+++ cmd/hermes/hermes.go
@@ -1,13 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"hermes/internal/hermes"
+	"log"
+	"os"
+	"runtime/pprof"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
 func main() {
+	flag.Parse()
 	fmt.Println("Hermes (Ἑρμῆς) - An Key-Val data store")
+
+	if *cpuprofile != "" {
+		fmt.Println("Profiling enabled")
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+
+		h := hermes.New(":3333")
+		go h.Listen()
+		var input string
+		fmt.Scanln(&input)
+		fmt.Println("Shutting down...")
+		return
+	}
 
 	h := hermes.New(":3333")
 	h.Listen()
+
 }
