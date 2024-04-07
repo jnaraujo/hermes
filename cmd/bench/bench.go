7b1dcef9ffec989350ab931594937f5adcaa59fa
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-04-07 11:52:26 -0300

    feat update benchmark code to increase total requests and concurrency

--- cmd/bench/bench.go
+++ cmd/bench/bench.go
@@ -3,51 +3,54 @@ package main
 import (
 	"fmt"
 	"hermes/internal/data"
-	"hermes/internal/hermes"
 	"net"
 	"sync"
 	"time"
 )
 
 const (
-	reqs = 10_000
+	total = 100_000
+	conc  = 100
 )
 
 func main() {
 	var wg sync.WaitGroup
 
-	h := hermes.New(":3333")
-	go h.Listen()
-
-	time.Sleep(50 * time.Millisecond) // waits server
-
 	started := time.Now()
-	for i := 0; i < reqs; i++ {
+
+	for i := 0; i < conc; i++ {
 		wg.Add(1)
 
 		go func() {
 			defer wg.Done()
-			send(*data.New("SET", fmt.Sprintf("foo=bar-%d", i)))
+			for i := 0; i < total/conc; i++ {
+				send(*data.New("SET", fmt.Sprintf("foo=bar-%d", i)))
+			}
 		}()
 	}
+
 	wg.Wait()
 
 	fmt.Println("SET benchmark:")
-	fmt.Println("Req/s:", reqs/time.Since(started).Seconds())
+	fmt.Println("Req/s:", total/time.Since(started).Seconds())
 
 	started = time.Now()
-	for i := 0; i < reqs; i++ {
+
+	for i := 0; i < conc; i++ {
 		wg.Add(1)
 
 		go func() {
 			defer wg.Done()
-			send(*data.New("GET", "foo"))
+			for i := 0; i < total/conc; i++ {
+				send(*data.New("GET", fmt.Sprintf("foo-%d", i)))
+			}
 		}()
 	}
+
 	wg.Wait()
 
 	fmt.Println("GET benchmark:")
-	fmt.Println("Req/s:", reqs/time.Since(started).Seconds())
+	fmt.Println("Req/s:", total/time.Since(started).Seconds())
 }
 
 func send(d data.Data) *data.Data {
@@ -60,7 +63,7 @@ func send(d data.Data) *data.Data {
 
 	conn.Write([]byte(d.Decode()))
 
-	buff := make([]byte, 1024)
+	buff := make([]byte, 128)
 	n, _ := conn.Read(buff)
 
 	resp, _ := data.FromStr(string(buff[:n]))
